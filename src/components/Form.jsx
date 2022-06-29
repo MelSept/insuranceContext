801d7e727eba8e5fd97ceba12af8a69c7ff8cb09
MelSept <septurmelina@gmail.com>
2022-06-29 17:38:02 -0300

    se agregan formulas y helpers

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,29 @@
 import { BRANDS, YEARS, PLANS } from "../constants";
 import { Fragment } from "react";
 import useInsurance from "../hooks/useInsurance";
+import Error from "./Error";
 
 const Form = () => {
-  const { data, handleChangeDatos } = useInsurance();
+  const { data, handleChangeDatos, setError, error, insuranceProvider } =
+    useInsurance();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // si los campos estan vacios, arroja un msj de error
+    if (Object.values(data).includes("")) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+
+    setError("");
+    insuranceProvider();
+  };
+
   return (
     <>
-      <form>
+      {error && <Error />}
+      <form onSubmit={handleSubmit}>
         <div className="my-5">
           <label className="block mb-3 font-bold text-gray-400 uppercase">
             Marca
--- src/context/InsuranceProvider.jsx
+++ src/context/InsuranceProvider.jsx
@@ -1,4 +1,10 @@
 import { useState, createContext } from "react";
+import {
+  getDifferenceYear,
+  calculateMarca,
+  calculatePlan,
+  moneyFormat,
+} from "../helpers";
 
 // children son todos los componentes hijos de la aplicacion
 
@@ -10,6 +16,9 @@ const InsuranceProvider = ({ children }) => {
     plan: "",
   });
 
+  const [error, setError] = useState("");
+  const [result, setResult] = useState(0);
+
   const handleChangeDatos = (e) => {
     setData({
       //toma una copia de lo que hay en data y lo sobre escribe
@@ -18,11 +27,38 @@ const InsuranceProvider = ({ children }) => {
     });
   };
 
+  const insuranceProvider = () => {
+    //base
+    let result = 2000;
+    //obtener diferencia de años
+    const difference = getDifferenceYear(data.year);
+
+    console.log(difference);
+
+    //Hay que restar el 3% por cada año
+    result -= (difference * 3 * result) / 100;
+    //Americano +15%
+    //Europeo +30%
+    //Asiatico +5%
+    result *= calculateMarca(data.marca);
+
+    //Basico +20%
+    //Completo +50%
+    result *= calculatePlan(data.plan);
+
+    //Formatear dinero
+    result = moneyFormat(result);
+    setResult(result);
+  };
+
   return (
     <InsuranceContext.Provider
       value={{
         data,
         handleChangeDatos,
+        error,
+        setError,
+        insuranceProvider,
       }}
     >
       {" "}
